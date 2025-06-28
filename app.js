d275fa109c5ab0916bd15fc02647afb65f46f18b
Haider Ali Siddiqui <haideralisiddiqui008@gmail.com>
2025-06-28 11:24:33 +0500

    html js jpg png file added

---
+++ app.js
@@ -0,0 +1,20 @@
+var h1El = document.getElementById("h1");
+var bulbEl = document.getElementById("bulb");
+var btnEl = document.getElementById("btn");
+var isOpen = false
+
+function handleBulbOnOff() {
+   if(isOpen) {
+    isOpen = false;
+    bulbEl.src = "./image/bulb off.jpg"
+    h1El.innerHTML = "Bulb off"
+    btnEl.innerHTML = "Bulb on"
+   }
+   else {
+    isOpen = true;
+    bulbEl.src = "./image/bulb on.png"
+    h1El.innerHTML = "Bulb on"
+    btnEl.innerHTML = "Bulb off"
+   }
+//    console.log(isOpen);
+}
